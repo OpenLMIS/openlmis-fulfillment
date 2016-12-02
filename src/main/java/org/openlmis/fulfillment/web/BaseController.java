79a4d5d42a06574cab59c5f6267a8313b22ae019
Łukasz Lewczyński <llewczynski@soldevelo.com>
2016-12-02 09:16:06 +0100

    Modified the way how a map of binding errors are created

--- src/main/java/org/openlmis/fulfillment/web/BaseController.java
+++ src/main/java/org/openlmis/fulfillment/web/BaseController.java
@@ -4,19 +4,17 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequestMapping("/api")
 public abstract class BaseController {
 
   protected Map<String, String> getErrors(BindingResult bindingResult) {
-    return new HashMap<String, String>() {
-      {
-        for (FieldError error : bindingResult.getFieldErrors()) {
-          put(error.getField(), error.getCode());
-        }
-      }
-    };
+    return bindingResult
+        .getFieldErrors()
+        .stream()
+        .collect(Collectors.toMap(FieldError::getField, FieldError::getCode));
   }
+
 }
