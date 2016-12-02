281e58d772bd6e433c725e5ea8c3f4e9564ca582
Łukasz Lewczyński <llewczynski@soldevelo.com>
2016-12-02 08:32:46 +0100

    Made a final properties in ReferenceDataRetrievalException

--- src/main/java/org/openlmis/fulfillment/referencedata/service/ReferenceDataRetrievalException.java
+++ src/main/java/org/openlmis/fulfillment/referencedata/service/ReferenceDataRetrievalException.java
@@ -8,16 +8,11 @@ import lombok.Getter;
  * Signals we were unable to retrieve reference data
  * due to a communication error.
  */
+@Getter
 public class ReferenceDataRetrievalException extends RuntimeException {
-
-  @Getter
-  private String resource;
-
-  @Getter
-  private HttpStatus status;
-
-  @Getter
-  private String response;
+  private final String resource;
+  private final HttpStatus status;
+  private final String response;
 
   /**
    * Constructs the exception.
@@ -26,8 +21,7 @@ public class ReferenceDataRetrievalException extends RuntimeException {
    * @param status   the http status that was returned
    * @param response the response from referencedata service
    */
-  public ReferenceDataRetrievalException(String resource,
-                                         HttpStatus status, String response) {
+  ReferenceDataRetrievalException(String resource, HttpStatus status, String response) {
     super(String.format("Unable to retrieve %s. Error code: %d, response message: %s",
         resource, status.value(), response));
     this.resource = resource;
