f311bf29dabf4cb23c47c4f80c29fce3f6ef59f0
Łukasz Lewczyński <llewczynski@soldevelo.com>
2016-12-06 08:44:48 +0100

    OLMIS-1319: Removed applicationContext.xml (FEOLMIS-617)

--- src/main/java/org/openlmis/fulfillment/Application.java
+++ src/main/java/org/openlmis/fulfillment/Application.java
@@ -1,19 +1,29 @@
 package org.openlmis.fulfillment;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.openlmis.fulfillment.i18n.ExposedMessageSource;
 import org.openlmis.fulfillment.i18n.ExposedMessageSourceImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 import java.util.Locale;
 
+@Configuration
 @SpringBootApplication
-@ImportResource("applicationContext.xml")
+@ComponentScan("org.openlmis.fulfillment")
 public class Application {
+
+  @Value("${defaultLocale}")
+  private Locale defaultLocale;
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
@@ -28,7 +38,7 @@ public class Application {
   public LocaleResolver localeResolver() {
     CookieLocaleResolver lr = new CookieLocaleResolver();
     lr.setCookieName("lang");
-    lr.setDefaultLocale(Locale.ENGLISH);
+    lr.setDefaultLocale(defaultLocale);
     return lr;
   }
 
@@ -45,6 +55,36 @@ public class Application {
     messageSource.setUseCodeAsDefaultMessage(true);
 
     return messageSource;
+  }
+
+  /**
+   * Creates new LocalValidatorFactoryBean.
+   *
+   * @return Created LocalValidatorFactoryBean.
+   */
+  @Bean
+  public LocalValidatorFactoryBean validator() {
+    return new LocalValidatorFactoryBean();
+  }
+
+  /**
+   * Creates new camelContext.
+   *
+   * @return Created camelContext.
+   */
+  @Bean
+  public CamelContext camelContext() {
+    return new DefaultCamelContext();
+  }
+
+  /**
+   * Creates new camelTemplate.
+   *
+   * @return Created camelTemplate.
+   */
+  @Bean
+  public ProducerTemplate camelTemplate() {
+    return camelContext().createProducerTemplate();
   }
 
 }
