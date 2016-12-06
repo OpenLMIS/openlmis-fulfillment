7fee842fe0c5287f4f79dde0f816b828ce0ebdee
Łukasz Lewczyński <llewczynski@soldevelo.com>
2016-12-06 13:37:27 +0100

    Removed unused method from OrderableProductDto

--- src/main/java/org/openlmis/fulfillment/referencedata/model/OrderableProductDto.java
+++ src/main/java/org/openlmis/fulfillment/referencedata/model/OrderableProductDto.java
@@ -17,30 +17,4 @@ public class OrderableProductDto {
   private boolean roundToZero;
   private Set<ProgramProductDto> programs;
   private DispensableDto dispensable;
-
-  /**
-   * Returns the number of packs to order. For this OrderableProduct given a desired number of
-   * dispensing units, will return the number of packs that should be ordered.
-   *
-   * @param dispensingUnits # of dispensing units we'd like to order for
-   * @return the number of packs that should be ordered.
-   */
-  public long packsToOrder(long dispensingUnits) {
-    if (dispensingUnits <= 0 || packSize == 0) {
-      return 0;
-    }
-
-    long packsToOrder = dispensingUnits / packSize;
-    long remainderQuantity = dispensingUnits % packSize;
-
-    if (remainderQuantity > 0 && remainderQuantity >= packRoundingThreshold) {
-      packsToOrder += 1;
-    }
-
-    if (packsToOrder == 0 && !roundToZero) {
-      packsToOrder = 1;
-    }
-
-    return packsToOrder;
-  }
 }
