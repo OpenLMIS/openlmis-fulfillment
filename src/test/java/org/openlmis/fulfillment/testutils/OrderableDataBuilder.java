0c89e7bafb3157c99265e71ae2cef90fcba7678f
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2018-01-24 12:34:41 +0100

    OLMIS-3952: added builders for referencedata resources

---
+++ src/test/java/org/openlmis/fulfillment/testutils/OrderableDataBuilder.java
@@ -0,0 +1,63 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+package org.openlmis.fulfillment.testutils;
+
+import org.openlmis.fulfillment.service.referencedata.DispensableDto;
+import org.openlmis.fulfillment.service.referencedata.OrderableDto;
+import org.openlmis.fulfillment.service.referencedata.ProgramOrderableDto;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class OrderableDataBuilder {
+
+  private static int instanceNumber = 0;
+
+  private UUID id;
+  private String productCode;
+  private String fullProductName;
+  private long netContent;
+  private long packRoundingThreshold;
+  private boolean roundToZero;
+  private Set<ProgramOrderableDto> programs;
+  private DispensableDto dispensable;
+
+  /**
+   * Builder for {@link OrderableDto}.
+   */
+  public OrderableDataBuilder() {
+    instanceNumber++;
+
+    id = UUID.randomUUID();
+    productCode = "P" + instanceNumber;
+    fullProductName = "product" + instanceNumber;
+    netContent = 10;
+    packRoundingThreshold = 0;
+    roundToZero = true;
+    programs = new HashSet<>();
+    dispensable = new DispensableDto();
+  }
+
+  /**
+   * Builds instance of {@link OrderableDto}.
+   */
+  public OrderableDto build() {
+    OrderableDto orderable = new OrderableDto(productCode, fullProductName, netContent,
+        packRoundingThreshold, roundToZero, programs, dispensable);
+    orderable.setId(id);
+    return orderable;
+  }
+}
