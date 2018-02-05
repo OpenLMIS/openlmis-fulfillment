2ebd34ef4e06a3730371c661f9cd87dfa5023ad9
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-02-05 10:51:59 +0100

    OLMIS-3956: Fixed problem with applying migration

--- src/main/resources/db/migration/20180202092754358__modify_pod_schema.sql
+++ src/main/resources/db/migration/20180202092754358__modify_pod_schema.sql
@@ -16,13 +16,13 @@ INNER JOIN fulfillment.order_line_items AS oli ON o.id = oli.orderId
 INNER JOIN fulfillment.shipments AS s ON o.id = s.orderId;
 
 UPDATE fulfillment.proof_of_deliveries AS p
-SET p.shipmentId = s.id
+SET shipmentId = s.id
 FROM fulfillment.orders AS o, fulfillment.shipments AS s
 WHERE p.orderId = o.id AND o.id = s.orderId;
 
 -- update default POD status
 UPDATE fulfillment.proof_of_deliveries AS p
-SET p.status = 'CONFIRMED'
+SET status = 'CONFIRMED'
 FROM fulfillment.orders AS o
 WHERE o.status = 'RECEIVED';
 
