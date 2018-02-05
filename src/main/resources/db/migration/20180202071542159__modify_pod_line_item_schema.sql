f92d0fae6eb834e6f92b81fecc994535754d28fe
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-02-05 10:12:26 +0100

    OLMIS-3956: Added database migration

--- src/main/resources/db/migration/20180202071542159__modify_pod_line_item_schema.sql
+++ src/main/resources/db/migration/20180202071542159__modify_pod_line_item_schema.sql
@@ -1,14 +1,19 @@
+-- add new columns
+ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN orderableId uuid NOT NULL;
+ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN lotId uuid;
+ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN quantityAccepted integer;
+ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN vvmStatus character varying(255);
+ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN useVvm boolean NOT NULL DEFAULT FALSE;
+ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN quantityRejected integer;
+ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN rejectionReasonId uuid;
+
+-- update existing rows
+UPDATE fulfillment.proof_of_delivery_line_items SET quantityAccepted = quantityreceived;
+UPDATE fulfillment.proof_of_delivery_line_items SET quantityRejected = quantityreturned;
+
+-- remove unnecessary columns
 ALTER TABLE fulfillment.proof_of_delivery_line_items DROP COLUMN quantityreceived;
 ALTER TABLE fulfillment.proof_of_delivery_line_items DROP COLUMN quantityreturned;
 ALTER TABLE fulfillment.proof_of_delivery_line_items DROP COLUMN quantityshipped;
 ALTER TABLE fulfillment.proof_of_delivery_line_items DROP COLUMN replacedproductcode;
 ALTER TABLE fulfillment.proof_of_delivery_line_items DROP COLUMN orderlineitemid;
-
-ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN orderableId uuid NOT NULL;
-ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN lotId uuid NOT NULL;
-ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN quantityAccepted integer;
-ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN vvmStatus character varying(255);
-ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN useVvm boolean NOT NULL;
-ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN quantityRejected integer;
-ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN rejectionReasonId uuid;
-
--- src/main/resources/db/migration/20180202092754358__modify_pod_schema.sql
+++ src/main/resources/db/migration/20180202092754358__modify_pod_schema.sql
@@ -1,6 +1,33 @@
-ALTER TABLE fulfillment.proof_of_deliveries ADD COLUMN shipmentId uuid NOT NULL;
-ALTER TABLE fulfillment.proof_of_deliveries ADD COLUMN status character varying(255) NOT NULL;
+-- add new columns
+ALTER TABLE fulfillment.proof_of_deliveries ADD COLUMN shipmentId uuid;
+ALTER TABLE fulfillment.proof_of_deliveries ADD COLUMN status character varying(255) DEFAULT 'INITIATED';
 
 -- connect external orders with pod
+INSERT INTO fulfillment.shipments (id, orderid, shippedbyid, shippeddate, notes, extradata)
+SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), o.id, o.createdById, o.createdDate, NULL, '{"external": true}'::jsonb
+FROM fulfillment.proof_of_deliveries AS p
+INNER JOIN fulfillment.orders AS o ON o.id = p.orderId;
 
+INSERT INTO fulfillment.shipment_line_items (id, orderableid, lotid, quantityshipped, shipmentid)
+SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), oli.orderableId, NULL, oli.orderedQuantity, s.id
+FROM fulfillment.proof_of_deliveries AS p
+INNER JOIN fulfillment.orders AS o ON o.id = p.orderId
+INNER JOIN fulfillment.order_line_items AS oli ON o.id = oli.orderId
+INNER JOIN fulfillment.shipments AS s ON o.id = s.orderId;
+
+UPDATE fulfillment.proof_of_deliveries AS p
+SET p.shipmentId = s.id
+FROM fulfillment.orders AS o, fulfillment.shipments AS s
+WHERE p.orderId = o.id AND o.id = s.orderId;
+
+-- update default POD status
+UPDATE fulfillment.proof_of_deliveries AS p
+SET p.status = 'CONFIRMED'
+FROM fulfillment.orders AS o
+WHERE o.status = 'RECEIVED';
+
+-- set not null flag for shipmentId column
+ALTER TABLE fulfillment.proof_of_deliveries ALTER COLUMN shipmentId SET NOT NULL;
+
+-- remove unnecessary columns
 ALTER TABLE fulfillment.proof_of_deliveries DROP COLUMN orderid;
