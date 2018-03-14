6bb74bd86129e74d0c610cb149cee7a7de54336e
Chongsun Ahn <cahn922@yahoo.com>
2018-03-13 22:59:31 -0700

    OLMIS-4237 Fix faulty migration
    POD line items table added orderableId column and made it not nullable, but did not provide data migration to enter orderableId from existing data.

--- src/main/resources/db/migration/20180202071542159__modify_pod_line_item_schema.sql
+++ src/main/resources/db/migration/20180202071542159__modify_pod_line_item_schema.sql
@@ -1,11 +1,18 @@
 -- add new columns
-ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN orderableId uuid NOT NULL;
+ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN orderableId uuid;
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN lotId uuid;
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN quantityAccepted integer;
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN vvmStatus character varying(255);
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN useVvm boolean NOT NULL DEFAULT FALSE;
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN quantityRejected integer;
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN rejectionReasonId uuid;
+
+-- update existing rows for orderableid, then enforce NOT NULL
+UPDATE fulfillment.proof_of_delivery_line_items podli
+  SET orderableid = oli.orderableid
+  FROM fulfillment.order_line_items oli
+  WHERE oli.id = podli.orderlineitemid;
+ALTER TABLE fulfillment.proof_of_delivery_line_items ALTER COLUMN orderableId SET NOT NULL;
 
 -- update existing rows
 UPDATE fulfillment.proof_of_delivery_line_items SET quantityAccepted = quantityreceived;
