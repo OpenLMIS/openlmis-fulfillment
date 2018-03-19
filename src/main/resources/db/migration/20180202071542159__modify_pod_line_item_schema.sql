4f22e43c6e3bb2e43e0b8f13f642caaff1a6ac91
Chongsun Ahn <cahn922@yahoo.com>
2018-03-19 12:07:12 -0700

    OLMIS-4274 Move add orderableid column to its own migration
    To see if there are performance issues with the existing migration and in preparation of improving performance of this migration.

--- src/main/resources/db/migration/20180202071542159__modify_pod_line_item_schema.sql
+++ src/main/resources/db/migration/20180202071542159__modify_pod_line_item_schema.sql
@@ -1,18 +1,10 @@
 -- add new columns
-ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN orderableId uuid;
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN lotId uuid;
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN quantityAccepted integer;
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN vvmStatus character varying(255);
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN useVvm boolean NOT NULL DEFAULT FALSE;
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN quantityRejected integer;
 ALTER TABLE fulfillment.proof_of_delivery_line_items ADD COLUMN rejectionReasonId uuid;
-
--- update existing rows for orderableid, then enforce NOT NULL
-UPDATE fulfillment.proof_of_delivery_line_items podli
-  SET orderableid = oli.orderableid
-  FROM fulfillment.order_line_items oli
-  WHERE oli.id = podli.orderlineitemid;
-ALTER TABLE fulfillment.proof_of_delivery_line_items ALTER COLUMN orderableId SET NOT NULL;
 
 -- update existing rows
 UPDATE fulfillment.proof_of_delivery_line_items SET quantityAccepted = quantityreceived;
@@ -23,4 +15,3 @@ ALTER TABLE fulfillment.proof_of_delivery_line_items DROP COLUMN quantityreceive
 ALTER TABLE fulfillment.proof_of_delivery_line_items DROP COLUMN quantityreturned;
 ALTER TABLE fulfillment.proof_of_delivery_line_items DROP COLUMN quantityshipped;
 ALTER TABLE fulfillment.proof_of_delivery_line_items DROP COLUMN replacedproductcode;
-ALTER TABLE fulfillment.proof_of_delivery_line_items DROP COLUMN orderlineitemid;
