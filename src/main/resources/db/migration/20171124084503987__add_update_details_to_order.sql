22c0359b5646aac30814c5960a7d1fc4f07e25ac
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-03-30 13:17:04 +0200

    OLMIS-4473: set lastupdateddate and lastupdaterid as not nullable

--- src/main/resources/db/migration/20171124084503987__add_update_details_to_order.sql
+++ src/main/resources/db/migration/20171124084503987__add_update_details_to_order.sql
@@ -3,3 +3,6 @@ ALTER TABLE fulfillment.orders ADD COLUMN lastupdaterid uuid;
 
 UPDATE fulfillment.orders SET lastupdateddate = createddate;
 UPDATE fulfillment.orders SET lastupdaterid = createdbyid;
+
+ALTER TABLE fulfillment.orders ALTER COLUMN lastupdateddate SET NOT NULL;
+ALTER TABLE fulfillment.orders ALTER COLUMN lastupdaterid SET NOT NULL;
