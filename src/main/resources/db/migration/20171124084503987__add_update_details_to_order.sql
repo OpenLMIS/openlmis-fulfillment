464f2fe57d3de67659309e71eabfcdd5e990704b
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-03-30 10:50:59 +0200

    OLMIS-4473: set default value for lastupdateddate and lastupdaterid

--- src/main/resources/db/migration/20171124084503987__add_update_details_to_order.sql
+++ src/main/resources/db/migration/20171124084503987__add_update_details_to_order.sql
@@ -1,2 +1,5 @@
 ALTER TABLE fulfillment.orders ADD COLUMN lastupdateddate timestamp with time zone;
 ALTER TABLE fulfillment.orders ADD COLUMN lastupdaterid uuid;
+
+UPDATE fulfillment.orders SET lastupdateddate = createddate;
+UPDATE fulfillment.orders SET lastupdaterid = createdbyid;
