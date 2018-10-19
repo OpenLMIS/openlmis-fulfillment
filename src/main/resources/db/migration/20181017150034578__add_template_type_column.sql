f8642914bdb781c40256825a052e6fcd1d29b660
Elias Muluneh <eliasmu@gmail.com>
2018-10-19 09:25:58 -0400

    OLMIS-5480: Added unique constraint, updated demo data

--- src/main/resources/db/migration/20181017150034578__add_template_type_column.sql
+++ src/main/resources/db/migration/20181017150034578__add_template_type_column.sql
@@ -7,3 +7,10 @@ UPDATE transfer_properties
 ALTER TABLE transfer_properties
   ALTER COLUMN transferType SET NOT NULL;
 
+ALTER TABLE transfer_properties
+  DROP CONSTRAINT IF EXISTS uk_sprkvmtubsjd58jc0afdycmiy;
+
+CREATE UNIQUE INDEX
+  uk_tp_by_facility_and_transfer_type
+  ON transfer_properties (transferType, facilityId);
+
