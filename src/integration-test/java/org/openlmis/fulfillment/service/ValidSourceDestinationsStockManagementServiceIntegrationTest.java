d292674494312c2dfa19feddc2491d447bfa4b71
hdsldv <88383112+hdsldv@users.noreply.github.com>
2022-02-20 22:10:13 +0100

    OLMIS-7530 Adjust tests for ValidDestinationsStockManagementService

--- src/integration-test/java/org/openlmis/fulfillment/service/ValidSourceDestinationsStockManagementServiceIntegrationTest.java
+++ src/integration-test/java/org/openlmis/fulfillment/service/ValidSourceDestinationsStockManagementServiceIntegrationTest.java
@@ -2,7 +2,7 @@
  * This program is part of the OpenLMIS logistics management information system platform software.
  * Copyright © 2017 VillageReach
  *
- * This program is free software: you can redistribute it and/or modify it under the terms
+ * This program is free software:you can redistribute it and/or modify it under the terms
  * of the GNU Affero General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
  *
@@ -33,7 +33,7 @@ import org.openlmis.fulfillment.service.stockmanagement.ValidDestinationsStockMa
 import org.openlmis.fulfillment.web.BaseWebIntegrationTest;
 import org.openlmis.fulfillment.web.stockmanagement.ValidSourceDestinationDto;
 
-public class ValidDestinationsStockManagementServiceIntegrationTest extends BaseWebIntegrationTest {
+public class ValidSourceDestinationsStockManagementServiceIntegrationTest extends BaseWebIntegrationTest {
 
   @Inject
   private ValidDestinationsStockManagementService validDestinationsStockManagementService;
@@ -46,64 +46,64 @@ public class ValidDestinationsStockManagementServiceIntegrationTest extends Base
 
   private final UUID nonMatchingId = UUID.fromString("f91de7ba-f63a-49ae-b5a9-9d7746ef250e");
 
-  private static final String VALID_DESTINATIONS_RESPONSE = "{\n"
-      + "  \"content\": [\n"
-      + "      \"id\": \"8059d138-f34e-4286-bc79-4b8f60aecca6\",\n"
-      + "    {\n"
-      + "      \"programId\": \"10845cb9-d365-4aaa-badd-b4fa39c6a26a\",\n"
-      + "      \"facilityTypeId\": \"ac1d268b-ce10-455f-bf87-9c667da8f060\",\n"
-      + "      \"node\": {\n"
-      + "        \"id\": \"e89eaf68-50c1-47f2-b83a-5b51ffa2206e\",\n"
-      + "        \"referenceId\": \"13037147-1769-4735-90a7-b9b310d128b8\",\n"
-      + "        \"refDataFacility\": true\n"
-      + "      },\n"
-      + "      \"name\": \"Balaka District Hospital\",\n"
-      + "      \"isFreeTextAllowed\": false\n"
-      + "    },\n"
-      + "    {\n"
-      + "      \"id\": \"c51365aa-ad56-4f5e-a7f4-d1eded160d46\",\n"
-      + "      \"programId\": \"10845cb9-d365-4aaa-badd-b4fa39c6a26a\",\n"
-      + "      \"facilityTypeId\": \"ac1d268b-ce10-455f-bf87-9c667da8f060\",\n"
-      + "      \"node\": {\n"
-      + "        \"id\": \"bb551f22-66b1-487a-a87d-9083d9eb8d56\",\n"
-      + "        \"referenceId\": \"f87480a8-5f06-4152-8e9d-6b53762a917e\",\n"
-      + "        \"refDataFacility\": false\n"
-      + "      },\n"
-      + "      \"name\": \"CHW\",\n"
-      + "      \"isFreeTextAllowed\": true\n"
-      + "    },\n"
-      + "    {\n"
-      + "      \"id\": \"a44fcfa4-edd8-45a7-a578-fcf5ae7791b3\",\n"
-      + "      \"programId\": \"10845cb9-d365-4aaa-badd-b4fa39c6a26a\",\n"
-      + "      \"facilityTypeId\": \"ac1d268b-ce10-455f-bf87-9c667da8f060\",\n"
-      + "      \"node\": {\n"
-      + "        \"id\": \"e89eaf68-50c1-47f2-b83a-5b51ffa2206e\",\n"
-      + "        \"referenceId\": \"13037147-1769-4735-90a7-b9b310d128b8\",\n"
-      + "        \"refDataFacility\": true\n"
-      + "      },\n"
-      + "      \"name\": \"Balaka District Hospital\",\n"
-      + "      \"isFreeTextAllowed\": false\n"
-      + "    }\n"
-      + "  ],\n"
-      + "  \"pageable\": {\n"
-      + "    \"sort\": [\n"
-      + "    ],\n"
-      + "    \"pageNumber\": 0,\n"
-      + "    \"pageSize\": 2147483647,\n"
-      + "    \"offset\": 0,\n"
-      + "    \"unpaged\": false,\n"
-      + "    \"paged\": true\n"
-      + "  },\n"
-      + "  \"totalElements\": 3,\n"
-      + "  \"last\": false,\n"
-      + "  \"totalPages\": 1,\n"
-      + "  \"first\": true,\n"
-      + "  \"sort\": [\n"
-      + "  ],\n"
-      + "  \"numberOfElements\": 3,\n"
-      + "  \"size\": 2147483647,\n"
-      + "  \"number\": 0,\n"
-      + "  \"empty\": false\n"
+  private static final String VALID_DESTINATIONS_RESPONSE = "{"
+      + "\"content\":["
+      + "\"id\":\"8059d138-f34e-4286-bc79-4b8f60aecca6\","
+      + "{"
+      + "\"programId\":\"10845cb9-d365-4aaa-badd-b4fa39c6a26a\","
+      + "\"facilityTypeId\":\"ac1d268b-ce10-455f-bf87-9c667da8f060\","
+      + "\"node\":{"
+      + "\"id\":\"e89eaf68-50c1-47f2-b83a-5b51ffa2206e\","
+      + "\"referenceId\":\"13037147-1769-4735-90a7-b9b310d128b8\","
+      + "\"refDataFacility\":true"
+      + "},"
+      + "\"name\":\"Balaka District Hospital\","
+      + "\"isFreeTextAllowed\":false"
+      + "},"
+      + "{"
+      + "\"id\":\"c51365aa-ad56-4f5e-a7f4-d1eded160d46\","
+      + "\"programId\":\"10845cb9-d365-4aaa-badd-b4fa39c6a26a\","
+      + "\"facilityTypeId\":\"ac1d268b-ce10-455f-bf87-9c667da8f060\","
+      + "\"node\":{"
+      + "\"id\":\"bb551f22-66b1-487a-a87d-9083d9eb8d56\","
+      + "\"referenceId\":\"f87480a8-5f06-4152-8e9d-6b53762a917e\","
+      + "\"refDataFacility\":false"
+      + "},"
+      + "\"name\":\"CHW\","
+      + "\"isFreeTextAllowed\":true"
+      + "},"
+      + "{"
+      + "\"id\":\"a44fcfa4-edd8-45a7-a578-fcf5ae7791b3\","
+      + "\"programId\":\"10845cb9-d365-4aaa-badd-b4fa39c6a26a\","
+      + "\"facilityTypeId\":\"ac1d268b-ce10-455f-bf87-9c667da8f060\","
+      + "\"node\":{"
+      + "\"id\":\"e89eaf68-50c1-47f2-b83a-5b51ffa2206e\","
+      + "\"referenceId\":\"13037147-1769-4735-90a7-b9b310d128b8\","
+      + "\"refDataFacility\":true"
+      + "},"
+      + "\"name\":\"Balaka District Hospital\","
+      + "\"isFreeTextAllowed\":false"
+      + "}"
+      + "],"
+      + "\"pageable\":{"
+      + "\"sort\":["
+      + "],"
+      + "\"pageNumber\":0,"
+      + "\"pageSize\":2147483647,"
+      + "\"offset\":0,"
+      + "\"unpaged\":false,"
+      + "\"paged\":true"
+      + "},"
+      + "\"totalElements\":3,"
+      + "\"last\":false,"
+      + "\"totalPages\":1,"
+      + "\"first\":true,"
+      + "\"sort\":["
+      + "],"
+      + "\"numberOfElements\":3,"
+      + "\"size\":2147483647,"
+      + "\"number\":0,"
+      + "\"empty\":false"
       + "}";
 
   @Before
@@ -131,6 +131,7 @@ public class ValidDestinationsStockManagementServiceIntegrationTest extends Base
     );
   }
 
+  @Test
   public void shouldFindValidDestination() {
     UUID expectedValidDestinationId = UUID.fromString("8059d138-f34e-4286-bc79-4b8f60aecca6");
 
