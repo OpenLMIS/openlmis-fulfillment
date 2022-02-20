74346b22ce83575779852d28ceb95ef7ceb1723b
hdsldv <88383112+hdsldv@users.noreply.github.com>
2022-02-20 22:42:26 +0100

    OLMIS-7530 Ignore pmd violations for old code

--- src/integration-test/java/org/openlmis/fulfillment/web/BaseWebIntegrationTest.java
+++ src/integration-test/java/org/openlmis/fulfillment/web/BaseWebIntegrationTest.java
@@ -66,6 +66,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public abstract class BaseWebIntegrationTest {
 
   protected static final String ID = "id";
