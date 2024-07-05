220bd4e3106d312ef5127464875204d0544bc1dd
Szymon Radziszewski <sradziszewski@soldevelo.com>
2024-07-05 08:01:18 +0200

    OIS-14: Added configuration for Transifex CLI usage
    Changed --resource value
    Updated credentialsId in Jenkinsfile
    Updated the version of docker-dev used to v9

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Sync with Transifex
 /transifex/sync_transifex.sh \
-  --resource openlmis-fulfillment.messages \
+  --resource openlmis-fulfillment \
   --pattern 'src/main/resources/messages_<lang>.properties' \
   --source-file src/main/resources/messages_en.properties
 
