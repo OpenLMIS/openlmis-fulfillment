da76e54a66d8b444bdd144ca4cd3306358feef85
Piotr Wargulak <pwargulak@soldevelo.com>
2025-01-30 18:28:53 +0100

    Added unit test to ProofOfDeliveryController

--- src/test/java/org/openlmis/fulfillment/web/ProofOfDeliveryControllerTest.java
+++ src/test/java/org/openlmis/fulfillment/web/ProofOfDeliveryControllerTest.java
@@ -15,13 +15,19 @@
 
 package org.openlmis.fulfillment.web;
 
+import static java.time.ZonedDateTime.now;
+import static java.util.Collections.emptyList;
+import static java.util.Optional.of;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.UUID;
 import org.junit.Test;
@@ -29,21 +35,69 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.openlmis.fulfillment.domain.ProofOfDelivery;
+import org.openlmis.fulfillment.domain.ProofOfDeliveryStatus;
+import org.openlmis.fulfillment.domain.Shipment;
+import org.openlmis.fulfillment.repository.OrderRepository;
+import org.openlmis.fulfillment.repository.ProofOfDeliveryRepository;
+import org.openlmis.fulfillment.service.FulfillmentNotificationService;
+import org.openlmis.fulfillment.service.PermissionService;
+import org.openlmis.fulfillment.service.referencedata.OrderableReferenceDataService;
+import org.openlmis.fulfillment.service.referencedata.UserDto;
+import org.openlmis.fulfillment.service.stockmanagement.StockEventStockManagementService;
+import org.openlmis.fulfillment.testutils.ShipmentDataBuilder;
+import org.openlmis.fulfillment.util.AuthenticationHelper;
+import org.openlmis.fulfillment.util.DateHelper;
+import org.openlmis.fulfillment.web.stockmanagement.StockEventDto;
 import org.openlmis.fulfillment.web.util.ProofOfDeliveryDto;
+import org.openlmis.fulfillment.web.util.ProofOfDeliveryDtoBuilder;
+import org.openlmis.fulfillment.web.util.StockEventBuilder;
 import org.openlmis.fulfillment.web.validator.ProofOfDeliveryValidator;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.MapBindingResult;
 
+@SuppressWarnings("PMD.UnusedPrivateField")
 @RunWith(MockitoJUnitRunner.class)
 public class ProofOfDeliveryControllerTest {
   private static final String POD_OBJECT = "proofOfDelivery";
   private static final String TEST_FIELD = "testField";
   private static final String TEST_ERROR_MESSAGE = "This is error message from validator.";
+  private static final String TEST_PERSON_NAME = "testPerson";
 
   @Mock
   private ProofOfDeliveryValidator proofOfDeliveryValidator;
+
+  @Mock
+  private ProofOfDeliveryRepository proofOfDeliveryRepository;
+
+  @Mock
+  private OrderableReferenceDataService orderableReferenceDataService;
+
+  @Mock
+  private PermissionService permissionService;
+
+  @Mock
+  private AuthenticationHelper authenticationHelper;
+
+  @Mock
+  private DateHelper dateHelper;
+
+  @Mock
+  private OrderRepository orderRepository;
+
+  @Mock
+  private StockEventBuilder stockEventBuilder;
+
+  @Mock
+  private StockEventStockManagementService stockEventStockManagementService;
+
+  @Mock
+  private FulfillmentNotificationService fulfillmentNotificationService;
+
+  @Mock
+  private ProofOfDeliveryDtoBuilder dtoBuilder;
 
   @InjectMocks
   private ProofOfDeliveryController controller;
@@ -68,5 +122,41 @@ public class ProofOfDeliveryControllerTest {
       assertNotNull(expected.getMessage());
       assertTrue(expected.getMessage().contains(TEST_ERROR_MESSAGE));
     }
+  }
+
+  @Test
+  public void updateProofOfDeliveryShouldUpdateAndSubmitStockEvent() {
+    final UUID podId = UUID.fromString("697bc284-77be-4ab1-9c4e-32b86e7d80b4");
+
+    final Shipment shipment = new ShipmentDataBuilder().build();
+    final ProofOfDeliveryDto proofOfDeliveryDto = new ProofOfDeliveryDto();
+    proofOfDeliveryDto.setId(podId);
+    proofOfDeliveryDto.setShipment(shipment);
+    proofOfDeliveryDto.setStatus(ProofOfDeliveryStatus.CONFIRMED);
+    proofOfDeliveryDto.setLineItems(emptyList());
+    proofOfDeliveryDto.setDeliveredBy(TEST_PERSON_NAME);
+    proofOfDeliveryDto.setReceivedBy(TEST_PERSON_NAME);
+    proofOfDeliveryDto.setReceivedDate(LocalDate.now());
+
+    final ProofOfDelivery proofOfDelivery =
+        new ProofOfDelivery(shipment, ProofOfDeliveryStatus.INITIATED, emptyList(), null, null,
+            null);
+
+    final BindingResult dummyBindingResult = new MapBindingResult(new HashMap<>(), POD_OBJECT);
+    final OAuth2Authentication dummyAuthentication = mock(OAuth2Authentication.class);
+
+    final StockEventDto stockEventDto = new StockEventDto();
+
+    when(proofOfDeliveryRepository.findById(podId)).thenReturn(of(proofOfDelivery));
+    when(authenticationHelper.getCurrentUser()).thenReturn(mock(UserDto.class));
+    when(dateHelper.getCurrentDateTimeWithSystemZone()).thenReturn(now());
+    when(stockEventBuilder.fromProofOfDelivery(proofOfDelivery)).thenReturn(of(stockEventDto));
+
+    controller
+        .updateProofOfDelivery(proofOfDeliveryDto.getId(), proofOfDeliveryDto, dummyBindingResult,
+            dummyAuthentication);
+
+    verify(proofOfDeliveryRepository).save(proofOfDelivery);
+    verify(stockEventStockManagementService).submit(stockEventDto);
   }
 }
